ec825944eced2fb9075cb7ebfb1a9d7e26e1e253
Görkem Takak <gorkem.takak@hotmail.com>
2024-12-21 14:06:23 +0300

    controlller örnek

---
+++ InternshipManagement/Controllers/PurchaseOrderController.cs
@@ -0,0 +1,29 @@
+﻿using Menager;
+using Menager.Dtos.RequestDto;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpareParts.Controllers
+{
+
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PurchaseOrderController : ControllerBase
+    {
+        private readonly ISpareParts _spareparts;
+
+        public PurchaseOrderController(ISpareParts spareParts)
+        {
+            _spareparts = spareParts;
+        }
+
+        [HttpPost]
+        public async Task CreatePurchaseOrder(CreatePurchaseOrderRequestDto requestDto)
+        {
+            await _spareparts.CreatePurchaseOrder(requestDto);
+        }
+
+
+    }
+}
--- InternshipManagement/obj/Debug/net8.0/SpareParts.AssemblyInfo.cs
+++ InternshipManagement/obj/Debug/net8.0/SpareParts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpareParts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+392952568dde3b0bc284d190b820e027f9bfa855")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c655bef9300ebff9593962d4cc343e8e337af90")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpareParts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpareParts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
